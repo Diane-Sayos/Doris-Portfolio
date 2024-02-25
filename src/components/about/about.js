c8a0fe57e19ee56d8440cf1f50aa290db4cedccf
Diane Sayos-Briones <dianesayos29@gmail.com>
2024-02-25 00:13:50 -0500

    styling for tabs trial

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,35 +1,128 @@
-/** @jsxImportSource @emotion/react */
-//dependencies
-// import { css } from '@emotion/react';
+// /** @jsxImportSource @emotion/react */
+// //dependencies
+// // import { css } from '@emotion/react';
 
-//components
+// //components
 
-//material ui
+// //material ui
 
-//files
+// //files
 
-//this is the fun description for doris with hobbies, soft skills, goal
+// //this is the fun description for doris with hobbies, soft skills, goal
+
+// const styles = {
+//     '.main-section': {
+//         margin: '1rem auto',
+//         display: 'flex',
+//         flexDirection: 'row wrap',
+//         border: '1px solid black',
+//         padding: 10,
+//     },
+//     '.about-section': {
+
+//     },
+// };
+
+
+// const About = () => {
+//     return (
+//         <section css={[styles?.['.main-section'], styles]} className='about-section'>
+//             <h2>About</h2>
+//         </section>
+//     )
+// };
+
+// export default About;
+
+import * as React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const styles = {
-    '.main-section': {
-        margin: '1rem auto',
+    '.MuiTabList-root': {
         display: 'flex',
-        flexDirection: 'row wrap',
-        border: '1px solid black',
-        padding: 10,
-    },
-    '.about-section': {
+        justifyContent: 'flex-end'
+    }
+}
 
-    },
-};
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
 
 const About = () => {
-    return (
-        <section css={[styles?.['.main-section'], styles]} className='about-section'>
-            <h2>About</h2>
-        </section>
-    )
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    <Box sx={{ bgcolor: 'background.paper', width: '100%' }}>
+      <AppBar position="static">
+        <Tabs
+          sx={{backgroundColor: '#7f5539'}}
+          value={value}
+          onChange={handleChange}
+          indicatorColor="white"
+          textColor="inherit"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
+    </Box>
+  );
 };
 
 export default About;
